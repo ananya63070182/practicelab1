54ff562b9f6f0f0c70c654b2e5f3fbe148b2553c
ananya63070182 <73657454+ananya63070182@users.noreply.github.com>
2020-11-26 11:25:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,55 @@
-<html>
-<head>
-<title>practicelab-1</title>
-</head>
-<body>
-<?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'practicelab1.mysql.database.azure.com', 'pxpleng@practicelab1', 'Pleng2545', 'itf', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
-?>
-<table width="600" border="1">
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
-  </tr>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
-  </tr>
-<?php
-}
-?>
-</table>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Practice Lab-1</title>
+  </head>
+  <body>
+    <?php
+    $conn = mysqli_init();
+    mysqli_real_connect($conn, 'practicelab1.mysql.database.azure.com', 'pxpleng@practicelab1', 'Pleng2545', 'itf', 3306);
+    if (mysqli_connect_errno($conn))
+    {
+        die('Failed to connect to MySQL: '.mysqli_connect_error());
+    }
+    $res = mysqli_query($conn, 'SELECT * FROM guestbook');
+    ?>
+        <div class="container">
+            <div class="row">
+                <table class="col-lg-9 col-md-9 col-sm-12">
+                    <table class="table-responsive"></table>
+                    <table class="table table-striped table-dark ">
+                        <thead>
+                            <tr>
+                                <th class="text-center" scope="col">Name</th>
+                                <th class="text-center" scope="col">Comment</th>
+                                <th class="text-center" scope="col">Link</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                while($Result = mysqli_fetch_array($res)) {
+                            
+                            ?>
+                            <tr>
+                                <td><?php echo $Result['Name'];?></td>
+                                <td><?php echo $Result['Comment'];?></td>
+                                <td><?php echo $Result['Link'];?></td>
+                            </tr>
+                            <?php
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+                </table>
+        </div>
+        </div>
 <?php
 mysqli_close($conn);
 ?>
